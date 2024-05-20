a8b42f6773d5c6711ba43878fbad0abb2c5a725e
WiktoriaSadloC:/Users/wikis/AppData/Local/Microsoft/WindowsApps/python3.10.exe <227960@student.uek.krakow.pl>
2024-05-20 22:40:56 +0200

    poprawki 20.05 WS

---
+++ CreditCard/src/main/java/ecommerce/sales/Offer.java
@@ -0,0 +1,13 @@
+package ecommerce.sales;
+
+import java.math.BigDecimal;
+
+public class Offer {
+    public BigDecimal getTotal() {
+        return BigDecimal.ZERO;
+    }
+
+    public int getItemsCount() {
+        return 0;
+    }
+}
